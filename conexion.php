8b41ef4b490203a848d21828869503c234e1f3b4
RuLy890 <racul890@gmail.com>
2025-05-20 23:01:28 -0700

    Cambios

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "u557447082_9x8vh";
 $password ="~s0yVXwZZ";
 
-/*
+
 $dbname = "u557447082_safegardedb";
 $conexion = new mysqli($servername, $username, $password, $dbname);
 
@@ -13,14 +13,17 @@ if ($conexion->connect_error) {
 }
 
 
+/*
+
 USAR ESTE SOLO PARA PRUEBAS LOCALES 
-*/
+
 
 $servername = "localhost";
 $username = "root";
-$password = "RuLy8902621";
+$password = "";
 $dbname = "safegar";
 $conexion = new mysqli($servername, $username, $password, $dbname);
 if ($conexion->connect_error) {
     die("Conexion fallida" . $conexion->connect_error);
 }
+/*
